ca444a72e38def46590e71677495dbea6d83c3ba
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-08-06 08:56:52 +0200

    Added Menu.swift

--- RetroVisor/AppDelegate.swift
+++ RetroVisor/AppDelegate.swift
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
-// This file is part of RetroVision
+// This file is part of RetroVisor
 //
 // Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
 // Licensed under the GNU General Public License v3
@@ -46,48 +46,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
-    func createStatusBarMenu() {
-
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
-        if let button = statusItem.button {
-            button.image = NSImage(named: "RetroVisorTemplate")!
-        }
-
-        let menu = NSMenu()
-
-        menu.addItem(NSMenuItem(
-            title: "Live Dragging",
-            action: #selector(liveDraggingAction(_:)),
-            keyEquivalent: ""
-        ))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(
-            title: "Restart Screen Capturer",
-            action: #selector(restartScreenCapturer(_:)),
-            keyEquivalent: ""
-        ))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(
-            title: "Quit \(Bundle.main.appName)",
-            action: #selector(NSApplication.terminate(_:)),
-            keyEquivalent: ""
-        ))
-
-        statusItem.menu = menu
-    }
-
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
-    }
-
-    @objc func restartScreenCapturer(_ sender: Any?) {
-        print("🔁 Restart Screen Capturer clicked")
-        // Your restart logic goes here
-    }
-
-    @objc func liveDraggingAction(_ sender: Any?) {
-        print("🔁 liveDraggingAction")
     }
 
     @IBAction func showSettings(_ sender: Any?) {
