ce2d8e350f94d7cc021ec00ba824e7fec4c83767
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-08-06 08:47:07 +0200

    Added StatusBarMenu (not working yet)

--- RetroVisor/AppDelegate.swift
+++ RetroVisor/AppDelegate.swift
@@ -12,6 +12,8 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var statusItem: NSStatusItem!
+
     // Window controller of the settings dialog
     var settingsWindowController: SettingsWindowController?
 
@@ -39,11 +41,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let frame = NSRect(x: 300, y: 300, width: 800, height: 600)
             window.setFrame(frame, display: true)
             window.makeKeyAndOrderFront(nil)
+
+            createStatusBarMenu()
         }
+    }
+
+    func createStatusBarMenu() {
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem.button {
+            button.image = NSImage(named: "RetroVisorTemplate")!
+        }
+
+        let menu = NSMenu()
+
+        menu.addItem(NSMenuItem(
+            title: "Live Dragging",
+            action: #selector(liveDraggingAction(_:)),
+            keyEquivalent: ""
+        ))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(
+            title: "Restart Screen Capturer",
+            action: #selector(restartScreenCapturer(_:)),
+            keyEquivalent: ""
+        ))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(
+            title: "Quit \(Bundle.main.appName)",
+            action: #selector(NSApplication.terminate(_:)),
+            keyEquivalent: ""
+        ))
+
+        statusItem.menu = menu
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
+    }
+
+    @objc func restartScreenCapturer(_ sender: Any?) {
+        print("🔁 Restart Screen Capturer clicked")
+        // Your restart logic goes here
+    }
+
+    @objc func liveDraggingAction(_ sender: Any?) {
+        print("🔁 liveDraggingAction")
     }
 
     @IBAction func showSettings(_ sender: Any?) {
@@ -54,4 +98,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         settingsWindowController?.showWindow(self)
     }
 
+}
+
+extension Bundle {
+
+    var appName: String {
+        object(forInfoDictionaryKey: "CFBundleName") as? String ?? "App"
+    }
 }
