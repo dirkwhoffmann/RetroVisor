ac44c946c71723a6540989d5c07f31a0feb1927f
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-08-10 16:47:22 +0200

    Capture mode can be changed in Settings

--- RetroVisor/Preferences/PreferencesViewControllers.swift
+++ RetroVisor/Preferences/PreferencesViewControllers.swift
@@ -1,18 +1,36 @@
 import Cocoa
 
 class GeneralPreferencesViewController: NSViewController {
-    /*
-    override func loadView() {
-        self.view = NSView()
-        let label = NSTextField(labelWithString: "General Preferences")
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+
+    @IBOutlet weak var fullCaptureButton: NSButton!
+    @IBOutlet weak var areaCaptureButton: NSButton!
+
+    override func viewDidLoad() {
+
+        print("GeneralPreferencesViewController.viewDidLoad")
+        refresh()
     }
-    */
+
+    var appDelegate: AppDelegate { NSApp.delegate as! AppDelegate }
+    var recorder: ScreenRecorder? { appDelegate.recorder }
+
+    func refresh() {
+
+        fullCaptureButton.state = recorder?.responsive == true ? .on : .off
+        areaCaptureButton.state = recorder?.responsive == false ? .on : .off
+    }
+
+    @IBAction func fullCaptureButton(_ sender: NSButton) {
+
+        recorder?.responsive = sender.state == .on
+        refresh()
+    }
+
+    @IBAction func areaCaptureButton(_ sender: NSButton) {
+
+        recorder?.responsive = sender.state == .off
+        refresh()
+    }
 }
 
 class ShaderPreferencesViewController: NSViewController {
